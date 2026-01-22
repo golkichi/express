9ac733d5b4daa39ef4226bf806415f1a94bfd6db
Golkichi <qoo.0509@gmail.com>
2026-01-22 17:04:15 +0900

    change messege

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Expressです' });
+  res.render('index', { title: 'Express 2' });
 });
 
 module.exports = router;
