972d27b9d2faf43e08ca71f04f7763afbf53800f
Golkichi <qoo.0509@gmail.com>
2026-01-29 02:08:12 +0900

    Final change for env file style

--- routes/notes_from_b.js
+++ routes/notes_from_b.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var router = express.Router();
 const cors = require('cors'); // corsミドルウェアを追加
+require('dotenv').config();
 
 
 // 接続情報を設定
 const { MongoClient } = require("mongodb");
-const uri = "mongodb+srv://takeshi_admin:kanayama01@cluster202601.azrvfsr.mongodb.net/?appName=Cluster202601";
+const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri);
 
 // corsミドルウェアを使用
